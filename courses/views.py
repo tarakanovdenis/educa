b069fea57bc55f90f3399a0a710137f54d4c3b8d
denis <tarakanov021098@gmail.com>
2023-11-18 20:12:26 +0500

    Enrolling on courses - 589 page

--- courses/views.py
+++ courses/views.py
@@ -9,11 +9,33 @@ from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
+from django.db.models import Count
+from django.views.generic.detail import DetailView
 from braces.views import CsrfExemptMixin, JSONRequestResponseMixin
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 
 
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
+    
+     
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subjects=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+        
+            
+            
 class ModuleOrderView(CsrfExemptMixin,
                       JSONRequestResponseMixin,
                       View):
